b085eac7b1056667de581ca2d9dd7d79ce619339
Idan <iconappmaster@gmail.com>
2020-12-21 10:53:42 +0200

    f

--- lib/data/sources/remote/rest/header_interceptor.dart
+++ lib/data/sources/remote/rest/header_interceptor.dart
@@ -36,8 +36,6 @@ class HeaderInterceptor extends Interceptor {
         error.response?.statusCode == 401) {
       dio.interceptors.requestLock.unlock();
       dio.interceptors.responseLock.unlock();
-      // replace this with login
-
       sl<AuthStore>().logout(false);
     } else {
       return error;
